f12d75343881913e841c6c647f6e6edb2959c362
Chelsea Otakan <chexee@gmail.com>
2011-02-21 10:13:19 -0800

    removed business-turnkey from this repo.

--- business-turnkey/searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-	<input id="search" name="s" type="text" value="Enter search terms and hit enter" onfocus="this.value=''" />
-</form>
